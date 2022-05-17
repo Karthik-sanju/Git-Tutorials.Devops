08caaaf4a61898ad3b8b761061f32680ed947ce9
karthik <mindtreejanedge27@iiht.tech>
2022-05-17 17:36:46 +0530

    tools

---
+++ demo/demo/src/main/java/com/example/demo/KktiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KktiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KktiApplication.class, args);
+	}
+
+}
